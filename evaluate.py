d449f451a3d484948e2565e3cb1e4a9ce98836dd
ml9951 <ml9951@rit.edu>
2018-03-19 14:21:56 -0400

    Upload weights to s3 when evaluating models

--- evaluate.py
+++ evaluate.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python
 
-import torch, os, json, argparse, pdb, numpy as np, pandas, time
+import os, json, argparse, pdb, numpy as np, pandas, time, importlib, psycopg2, boto3
 from shapely.geometry import MultiPolygon, box
-from TensorBox.predict import TensorBox
-from faster_rcnn_pytorch.predict import FasterRCNN
 from subprocess import check_output
 from precision_recall import precision_recall
 from tqdm import tqdm
+from model import MODELS
+from zipfile import ZipFile
+from dotenv import load_dotenv, find_dotenv
+load_dotenv(find_dotenv())
 
 def get_results(args, model):
     all_predictions = []
@@ -24,15 +26,54 @@ def get_results(args, model):
     f1 = 2 * (precision[i] * recall[i]) / (precision[i] + recall[i])
     print('precision = %f, recall = %f, F1 = %f' % (precision[i], recall[i], f1))
 
-    return df
+    summary = {
+        'precision' : precision[i],
+        'recall' : recall[i],
+        'f1' : f1,
+        'threshold' : float(df.sort_values(by='score', ascending=False)['score'].values[i]),
+        'name' : args.model_class.name,
+        'id' : args.model_class.mk_hash(args.weights),
+        'weights' : os.path.basename(args.weights),
+        'test_boxes' : args.test_boxes
+    }
+
+    return df, summary
+
+def upload_model(args, summary):
+    zip_file = args.model_class.zip_weights(args.weights, base_dir='weights')
+    s3 = boto3.resource('s3')
+    key = os.path.join('building-detection', os.path.basename(zip_file))
+
+    qargs = {k : summary[k] for k in ['name', 'id', 'precision', 'recall', 'threshold']}
+    qargs['instance'] = '/'.join(args.weights.split('/')[-2:])
+    qargs['s3_loc'] = os.path.join('s3://aigh-deep-learning-models/', key)
+
+    conn = psycopg2.connect(
+        dbname=os.environ.get('PGDATABASE', 'aigh'),
+        user=os.environ.get('PGUSER', ''),
+        password=os.environ.get('PGPASSWORD', ''),
+        host=os.environ.get('PGHOST', '')
+    )
+
+    with conn.cursor() as cur:
+        cur.execute("""
+            INSERT INTO models(name, instance, id, tested_on, precision, recall, threshold, s3_loc)
+            VALUES (%(name)s, %(instance)s, %(id)s, now(), %(precision)s, %(recall)s, %(threshold)s, %(s3_loc)s)
+        """, qargs)
+
+        json.dump(summary, open('.summary.json', 'w'))
+        with ZipFile(zip_file, 'a') as z:
+            z.write('.summary.json', '%s/description.json' % qargs['id'])
+
+        s3.meta.client.upload_file(zip_file, 'aigh-deep-learning-models', key)
+        conn.commit()
 
 def main():
     parser = argparse.ArgumentParser()    
     parser.add_argument('--test_boxes', required=True, help='Path to the JSON file containing the test set')
     parser.add_argument('--gpu', default=0)
-    parser.add_argument('--model', required=True, help='Which model to run (tensorbox, yolo, or rcnn)')
     parser.add_argument('--weights', default=None, help='Path to weight file (default is in description.json)')
-    parser.add_argument('--threshold', default=None, type=float, help='Confidence threshold (default is in description.json)')
+    parser.add_argument('--threshold', default=0.5, type=float, help='Confidence threshold (default is in description.json)')
     args = parser.parse_args()
 
     os.environ['CUDA_VISIBLE_DEVICES'] = str(args.gpu)
@@ -41,20 +82,19 @@ def main():
         os.mkdir('results')
 
     model = None
-    if args.model == 'tensorbox':
-        model = TensorBox(weights=args.weights)
-    elif args.model == 'faster-rcnn':
-        model = FasterRCNN(weights=args.weights)
-    else:
-        raise Exception('Model not recognized!')
+    for model_type in MODELS:
+        if model_type in args.weights:
+            module = importlib.import_module(model_type)
+            args.model_class = getattr(module, model_type)
+            model = args.model_class(args.weights)
+            model_id = args.model_class.mk_hash(args.weights)
 
-    description = json.load(open('weights/%s/description.json' % args.model))
+    if model is None:
+        raise ValueError('Invalid model')
 
-    if args.threshold is None:
-        args.threshold = description['threshold']
+    df, summary = get_results(args, model)
 
-    df = get_results(args, model)
-    df.to_csv('results/%s_predictions.csv' % args.model, index=False)
+    upload_model(args, summary)
 
 if __name__ == '__main__':
     main()
