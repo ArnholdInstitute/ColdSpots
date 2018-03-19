fbd83835ae17eb838438a9f3a2abe9b5ba6b81d0
ml9951 <ml9951@rit.edu>
2018-03-19 14:21:21 -0400

    Updating db file to also get ATLAS db config

--- db.py
+++ db.py
@@ -2,10 +2,18 @@ import psycopg2, os
 from dotenv import load_dotenv, find_dotenv
 load_dotenv(find_dotenv())
 
-db_args = {
-    'dbname' : 'aigh',
-    'host' : os.environ.get('PGHOST', 'localhost'),
-    'user' : os.environ.get('PGUSER', ''),
-    'password' : os.environ.get('PGPASSWORD', '')
+aigh_db_args = {
+    'dbname' : os.environ.get('AIGH_DATABASE', 'aigh'),
+    'host' : os.environ.get('AIGH_HOST', 'localhost'),
+    'user' : os.environ.get('AIGH_USER', ''),
+    'password' : os.environ.get('AIGH_PASSWORD', '')
 }
-conn = psycopg2.connect(**db_args)
+aigh_conn = psycopg2.connect(**aigh_db_args)
+
+atlas_db_args = {
+    'dbname' : os.environ.get('ATLAS_DATABASE', 'atlas'),
+    'host' : os.environ.get('ATLAS_HOST', 'localhost'),
+    'user' : os.environ.get('ATLAS_USER', ''),
+    'password' : os.environ.get('ATLAS_PASSWORD', '')
+}
+atlas_conn = psycopg2.connect(**atlas_db_args)
